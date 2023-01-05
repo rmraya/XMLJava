53b55c44ed520645a8fb5fff8a003bfac52c75d7
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-01-05 08:53:57 -0300

    Added build & version

---
+++ src/com/maxprograms/xml/Constants.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+ package com.maxprograms.xml;
+
+public class Constants {
+
+    public static final String VERSION = "1.0.0";
+    public static final String BUILD = "20230105_0852";
+}
