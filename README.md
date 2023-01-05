8a156d704d19b88108b78cec363aa6b7eea76262
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-01-05 08:47:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# XMLJava
+
+Custom library for handling XML in [Maxprograms](https://maxprograms.com) projects.
+
+## Building
+
+You need JAVA 17 and [Apache Ant 1.10.12](https://ant.apache.org) or newer
+
+- Checkout this repository
+- Point your JAVA_HOME variable to JDK 17
+- Run `ant` to compile the source code
---
+++ src/module-info.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Maxprograms.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 1.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors: Maxprograms - initial API and implementation
+ *******************************************************************************/
+module xmljava {
+	
+	exports com.maxprograms.xml;
+
+	opens com.maxprograms.xml to mapdb;
+
+	requires java.base;
+	requires transitive java.xml;
+}
