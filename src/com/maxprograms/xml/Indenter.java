ac74fba864ce3fbe6c6100dd58f91207efc2287a
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-03-04 15:12:36 -0300

    Updated license terms for release 4.0.0

--- src/com/maxprograms/xml/Indenter.java
+++ src/com/maxprograms/xml/Indenter.java
@@ -4,7 +4,6 @@
  * This software is the proprietary property of Maxprograms.
  * Use, modification, and distribution are subject to the terms of the 
  * Software License Agreement found in the root of this distribution 
- * and at http://www.maxprograms.com/
  *
  * Unauthorized redistribution or commercial use is strictly prohibited.
  *******************************************************************************/
